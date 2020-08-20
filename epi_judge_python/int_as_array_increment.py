a5208289f4cf1ac476a46ff598861e44ff846633
ntkjer <ntkjer@gmail.com>
2020-08-20 16:26:55 -0400

    pushing updates

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,9 +3,43 @@ from typing import List
 from test_framework import generic_test
 
 
+def plus_one_3(A: List[int]) -> List[int]:
+    carry = True
+    for i in reversed(range(len(A))):
+        while carry:
+            carry = False
+            value, carry = increment(A[i])
+            if i == 0 and (A[i] // 9 == 1):
+                tmp = [1]
+                A[i] = value
+                A = tmp + A
+                carry = False
+            else:
+                A[i] = value
+                break
+    return A
+
+
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
+
+
+def increment(x: int) -> (int, bool):
+    result, carry = 0, False
+    if x // 9 == 1:
+        result, carry = 0, True
+        return result, carry
+    result = x + 1
+    return result, carry
 
 
 if __name__ == '__main__':
--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,7 +4,41 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
+
+    # [1, 4]
+    # [1, 2]
+
+
+    # 1.
+    # [2, 8]
+    # [1, 4]
+    # [3, 12] or [4, 9]
+    # [4, 2]
+
+    # add last numbers
+    # if over 9, save 9 as A[i]
+    # append the sum, as if base 10 was not a boundary (e.g. 12)
+    # [3, 9, 2]
+
+    # [1, 2, 3]
+    # [1, 8]
+
+    # [ 8, 16, 24]
+    # [ 1, 2 , 3]
+
+    # [9, 18, 27]
+    # reduce
+    # [9 + 1, 9 + 2, 9, 7]
+    # reduce
+    # [10 + 1, 1 ,9 , 7]
+    # reduce
+    # [1, , 1, 9, 7]
+
+
+    choice = num1
+    if len(num1) <= len(num2):
+        choice = num2
+
     return []
 
 
