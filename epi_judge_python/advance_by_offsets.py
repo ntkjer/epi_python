c22e10a5a3cb09321bdf3151127261d5a93e3aa6
ntkjer <ntkjer@gmail.com>
2020-08-21 15:15:48 -0400

    advance_by_offsets

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    solved = len(A) - 1
+    max_moves = 0
+    i = 0
+    while i <= max_moves and max_moves < solved:
+        if i + A[i] > max_moves:
+            max_moves = i + A[i]
+            # max_moves = max(max_moves, A[i] + 1)
+        i += 1
+    return max_moves >= solved
 
 
 if __name__ == '__main__':
