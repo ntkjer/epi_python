4320b5372b4749e5741f1a1dbc209b163303b502
ntkjer <ntkjer@gmail.com>
2020-08-23 16:33:38 -0400

    small refactor

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -2,7 +2,6 @@ from typing import List
 
 from test_framework import generic_test
 
-import time
 def apply_permutation_bf(perm: List[int], A: List[int]) -> None:
     assert(len(perm) == len(A))
     result = [0] * len(A)
@@ -14,18 +13,10 @@ def apply_permutation_bf(perm: List[int], A: List[int]) -> None:
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    print("testing..")
-    print(A)
-    print(perm)
     for i in range(len(A)):
-        print("i: ", i, ", perm[i]:", perm[i], ", A[i]: ", A[i])
         while perm[i] != i:
-            print("A[perm[i]]: ", A[perm[i]], ", A[i]:", A[i])
             A[perm[i]], A[i] = A[i], A[perm[i]]
-            print("perm[perm[i]]: ", perm[perm[i]], ", perm[i]: ", perm[i])
             perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
-    print(A)
-    time.sleep(5)
 
 
 def apply_permutation_wrapper(perm, A):
