16d6ad9485cddd78a2fe3b186345076bd0cf5b7d
ntkjer <ntkjer@gmail.com>
2020-08-26 17:34:37 -0400

    problem mapping update

--- problem_mapping.js
+++ problem_mapping.js
@@ -348,7 +348,7 @@ problem_mapping = {
                 "total": 8
             },
             "Python: offline_sampling.py": {
-                "passed": 0,
+                "passed": 8,
                 "total": 8
             }
         },
@@ -376,7 +376,7 @@ problem_mapping = {
                 "total": 6
             },
             "Python: random_permutation.py": {
-                "passed": 0,
+                "passed": 6,
                 "total": 6
             }
         },
