d205805b7ae49176053132e4ead1e86c58b62b15
ntkjer <ntkjer@gmail.com>
2020-09-09 16:07:08 -0400

    pushing progress up

--- epi_judge_python/matrix_rotation.py
+++ epi_judge_python/matrix_rotation.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def rotate_matrix(square_matrix: List[List[int]]) -> None:
-    # TODO - you fill in here.
+
+    matrix_size = len(square_matrix) - 1
+    for i in range(len(square_matrix) // 2):
+        for j in range(i, matrix_size - i):
+            (square_matrix[i][j], square_matrix[~j][i], square_matrix[~i][~j],
+             square_matrix[j][~i]) = (square_matrix[~j][i],
+                                     square_matrix[~i][~j],
+                                     square_matrix[j][~i],
+                                     square_matrix[i][j])
     return
+
+
+class RotatedMatrix:
+    def __init__(self, square_matrix: List[List[int]]) -> None:
+        self._square_matrix = square_matrix
+
+    def read_entry(self, i: int, j: int) -> int:
+        return self._square_matrix[~j][i]
+
+    def write_entry(self, i: int,  j: int, v: int) -> None:
+        self._square_matrix[~j][i] = v
 
 
 def rotate_matrix_wrapper(square_matrix):
--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    # result = [[1] * (i + 1) for i in range(n)]
+    result = []
+    for i in range(n):
+        result.append([1] * (i + 1))
+
+    for i in range(len(result)):
+        for j in range(1, i):
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
+
+    return result
 
 
 if __name__ == '__main__':
