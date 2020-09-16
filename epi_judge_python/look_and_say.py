f7b3990ea028878ce25061bfd6f7f18b8f953123
ntkjer <ntkjer@gmail.com>
2020-09-16 09:10:31 -0400

    look and say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,11 +2,23 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    def next_item(s):
+        result, i = [], 0
+        while i < len(s):
+            count = 1
+            while i + 1 < len(s) and s[i] == s[i + 1]:
+                i += 1
+                count += 1
+            result.append(str(count) + s[i])
+            i += 1
+        return ''.join(result)
 
+    s = '1'
+    for _ in range(1, n):
+        s = next_item(s)
+
+    return s
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('look_and_say.py', 'look_and_say.tsv',
-                                       look_and_say))
