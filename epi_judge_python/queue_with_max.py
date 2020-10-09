6e33d54a957e36389142332d1d225fc1767546e9
ntkjer <ntkjer@gmail.com>
2020-10-09 11:07:01 -0400

    added q with max, brute-force dq

--- epi_judge_python/queue_with_max.py
+++ epi_judge_python/queue_with_max.py
@@ -1,21 +1,45 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+import collections
+import math
 
 class QueueWithMax:
 
     def __init__(self) -> None:
+        self._entries = collections.deque()
+        self._max = -math.inf
 
     def enqueue(self, x: int) -> None:
-        return
+        self._entries.append(x)
+        if not self._entries:
+            self._max = x
+        else:
+            self._max = max(x, self._max)
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        """
+        Brute force O(m) where m is the number of
+        queued items we check to update new max.
+        """
+        if self._entries:
+            result = self._entries.popleft()
+            if result is self._max:
+                curr_max = self._entries[-1] if self._entries else -math.inf
+                for entry in self._entries:
+                    if entry >= curr_max:
+                        curr_max = entry
+                self._max = curr_max
+            return result
+        raise ValueError('Cant dequeue empty queue')
+
+    def empty(self) -> bool:
+        return len(self._entries) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError("Empty Queue has no max")
+        return self._max
 
 
 def queue_tester(ops):
