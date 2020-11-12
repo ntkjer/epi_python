588f82beef0bcc2b91c657f6140943f2bba31122
ntkjer <ntkjer@gmail.com>
2020-11-12 18:52:36 -0500

    recursion progress

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,52 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    
-    return []
+    def nQueens(size):
+        board = [-1] * size
+        rQueens(board, 0, size)
+        print(board)
+
+    # This procedure checks that the most recently placed queen on column current
+    # does not conflict with queens in columns to the left.
+    def noConflicts(board, current):
+        for i in range(current):
+            if (board[i] == board[current]):
+                return False
+            if (current - i == abs(board[current] - board[i])):
+                return False
+        return True
+
+        # This procedure places a queens on the board on a given column so it does
+
+    # not conflict with the existing queens, and then calls itself recursively
+    # to place subsequent queens till the requisite number of queens are placed
+    def rQueens(board, current, size):
+        if (current == size):
+            return True
+        else:
+            for i in range(size):
+                board[current] = i
+                if (noConflicts(board, current)):
+                    done = rQueens(board, current + 1, size)
+                    if (done):
+                        return True
+            return False
+
+#
+#     def solve_n_queens(row):
+#         if row == n:
+#             result.append(col_placement.copy())
+#             return
+#         for col in range(n):
+#             if all(
+#                     abs(c - col) not in (0, row - i)
+#                     for i, c in enumerate(col_placement[:row])):
+#                 col_placement[row] = col
+#                 solve_n_queens(row + 1)
+#     result: List[List[int]] = []
+#     col_placement = [0] * n
+#     solve_n_queens(0)
+#     return result
 
 
 def comp(a, b):
--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -4,8 +4,19 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def permute(idx):
+        if idx == len(A) - 1:
+            result.append(A.copy())
+            return
+
+        for j in range(idx, len(A)):
+            A[idx], A[j] = A[j], A[idx]
+            permute(idx + 1)
+            A[idx], A[j] = A[j], A[idx]
+
+    result: List[List[int]] = []
+    permute(0)
+    return result
 
 
 if __name__ == '__main__':
