f615bedcd168684ead7c965da4491aef2411144b
ntkjer <ntkjer@gmail.com>
2020-11-22 21:00:43 -0500

    dp progress

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,40 +1,70 @@
 from test_framework import generic_test
 
 import functools
+import time
 
 def levenshtein_distance(A: str, B: str) -> int:
     # S A T U R D A Y
     # S U N D A Y
     # >> 4
 
-    # S  T U R D A Y
-    # S    U R D A Y
-    # S    U N D A Y
-    #    S U N D A Y
-    # S U N D A Y
-
-    # A C
-    # A B
-
     # if  l_d = E(A[0, a - 1], B[0, b -  1]), same last char
     # then l_d = E(A[0, a - 2], B[0, b - 2]), diff last char
-    @functools.lru_cache(None)
-    def compute_distance_between_prefixes(A_idx, B_idx):
-        if A_idx < 0:
-            # A is empty so add all of B's characters
-            return B_idx + 1
-        elif B_idx < 0:
-            # B is empty so delete all of A's chars
-            return A_idx + 1
-        if A[A_idx] == B[B_idx]:
-            return compute_distance_between_prefixes(A_idx - 1, B_idx - 1)
+    # in general we take the min of the current levenshetin dist + 1 out of three cases
 
-        subsitute_last = compute_distance_between_prefixes(A_idx - 1, B_idx - 1)
-        add_last = compute_distance_between_prefixes(A_idx, B_idx - 1)
-        delete_last = compute_distance_between_prefixes(A_idx - 1, B_idx)
-        return 1 + min(subsitute_last, add_last, delete_last)
+    # bottom-up
+    lookup = [[0 for i in range(len(B) + 1)] for j in range(len(A) + 1)]
+    for i in range(len(A) + 1):
+        lookup[i][0] = i
 
-    return compute_distance_between_prefixes(len(A) - 1, len(B) - 1)
+    for i in range(len(B) + 1):
+        lookup[0][i] = i
+
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            if A[i - 1] == B[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1]
+            else:
+                add_last = 1 + lookup[i][j - 1]
+                delete_last = 1 + lookup[i - 1][j]
+                sub_last = 1 + lookup[i - 1][j - 1]
+                lookup[i][j] = min(add_last, delete_last, sub_last)
+    return lookup[-1][-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     # top-down solution from book
+#     @functools.lru_cache(None)
+#     def compute_distance_between_prefixes(A_idx, B_idx):
+#         if A_idx < 0:
+#             # A is empty so add all of B's characters
+#             return B_idx + 1
+#         elif B_idx < 0:
+#             # B is empty so delete all of A's chars
+#             return A_idx + 1
+#         if A[A_idx] == B[B_idx]:
+#             return compute_distance_between_prefixes(A_idx - 1, B_idx - 1)
+#
+#         substitute_last = compute_distance_between_prefixes(A_idx - 1, B_idx - 1)
+#         add_last = compute_distance_between_prefixes(A_idx, B_idx - 1)
+#         delete_last = compute_distance_between_prefixes(A_idx - 1, B_idx)
+#         return 1 + min(substitute_last, add_last, delete_last)
+#
+#     return compute_distance_between_prefixes(len(A) - 1, len(B) - 1)
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,10 +1,24 @@
 from test_framework import generic_test
 
+import functools
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lookup = [[1 for _ in range(n)] for _ in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            lookup[i][j] = lookup[i - 1][j] + lookup[i][j - 1]
 
+    return lookup[-1][-1]
+
+def number_of_ways(n: int, m: int) -> int:
+    @functools.lru_cache(None)
+    def compute_number_of_ways_to_xy(x, y):
+        if x == y == 0:
+            return 1
+        ways_top = 0 if x == 0 else compute_number_of_ways_to_xy(x - 1, y)
+        ways_left = 0 if y == 0 else compute_number_of_ways_to_xy(x, y - 1)
+        return ways_left + ways_top
+    return compute_number_of_ways_to_xy(n - 1, m - 1)
 
 if __name__ == '__main__':
     exit(
