1d2294ce70761169a696961c4a49d0a4f00e41e7
ntkjer <ntkjer@gmail.com>
2020-12-01 20:05:55 -0500

    dp progress

--- epi_judge_python/minimum_weight_path_in_a_triangle.py
+++ epi_judge_python/minimum_weight_path_in_a_triangle.py
@@ -2,11 +2,36 @@ from typing import List
 
 from test_framework import generic_test
 
+import time
+
+def minimum_path_weight_book(triangle: List[List[int]]) -> int:
+    min_weight_to_curr_row = [0]
+    for row in triangle:
+        min_weight_to_curr_row = [
+            row[j] + min(
+                min_weight_to_curr_row[max(j - 1, 0)],
+                min_weight_to_curr_row[min(j, len(min_weight_to_curr_row) - 1)])
+            for j in range(len(row))
+        ]
+    return min(min_weight_to_curr_row)
 
 def minimum_path_weight(triangle: List[List[int]]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if not triangle:
+        return 0
 
+    res = [[0 for i in range(len(row))] for row in triangle]
+    res[0][0] = triangle[0][0]
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                # left adj
+                res[i][j] = res[i - 1][j] + triangle[i][j]
+            elif j == len(triangle[i]) - 1:
+                # right  adj
+                res[i][j] = res[i - 1][j - 1] + triangle[i][j]
+            else:
+                res[i][j] = min(res[i - 1][j - 1], res[i - 1][j]) + triangle[i][j]
+    return min(res[-1])
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python/picking_up_coins.py
+++ epi_judge_python/picking_up_coins.py
@@ -2,10 +2,23 @@ from typing import List
 
 from test_framework import generic_test
 
+import functools
 
 def maximum_revenue(coins: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    @functools.lru_cache(None)
+    def compute_max_revenue_for_range(a, b):
+        if a > b:
+            # no coins left
+            return 0
+        max_revenue_a = coins[a] + min(
+            compute_max_revenue_for_range(a + 2, b),
+            compute_max_revenue_for_range(a + 1, b - 1))
+        max_revenue_b = coins[b] + min(
+            compute_max_revenue_for_range(a + 1, b - 1),
+            compute_max_revenue_for_range(a, b - 2))
+        return max(max_revenue_a, max_revenue_b)
+
+    return compute_max_revenue_for_range(0, len(coins) - 1)
 
 
 if __name__ == '__main__':
